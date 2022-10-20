98a71ec71f0efb70125195c04626888c053a99ff
tabascum <marques_vasco@yahoo.es>
2022-10-20 15:27:19 +0100

    Upload project

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/* dropdown menu behaviour */
+
+const servicesBtn = document.querySelector(".nav-dropbtn");
+const services = document.querySelector(".nav-dropdown-content");
+
+servicesBtn.addEventListener("click", () => {
+  services.setAttribute;
+  services.classList.toggle("nav-dropdown-content.active");
+});
